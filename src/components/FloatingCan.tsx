34ba690b0a2cb16c42b85cc4e686e3ffa20b136c
tankmurdock <new.support@gmail.com>
2024-12-02 10:01:53 -0500

    Initial Commit everything up to Three Fiber part adding cans

---
+++ src/components/FloatingCan.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { Float } from "@react-three/drei";
+import { SodaCan, SodaCanProps } from "@/components/SodaCan";
+import { forwardRef, ReactNode } from "react";
+import { Group } from "three";
+
+type FloatingCanProps = {
+  flavor?: SodaCanProps["flavor"];
+  floatSpeed?: number;
+  rotationIntensity?: number;
+  floatIntensity?: number;
+  floatingRange?: [number, number];
+  children?: ReactNode;
+};
+
+const FloatingCan = forwardRef<Group, FloatingCanProps>(
+  (
+    {
+      flavor = "blackCherry",
+      floatSpeed = 1.5,
+      rotationIntensity = 1,
+      floatIntensity = 1,
+      floatingRange = [-0.1, 0.1],
+      children,
+      ...props
+    },
+    ref,
+  ) => {
+    return (
+      <group ref={ref} {...props}>
+        <Float
+          speed={floatSpeed}
+          floatIntensity={floatIntensity}
+          floatingRange={floatingRange}
+        >
+          {children}
+          <SodaCan />
+        </Float>
+      </group>
+    );
+  },
+);
+FloatingCan.displayName = "FLoatingCan";
+
+export default FloatingCan;
